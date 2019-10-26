81b303bc7dac51204ac5300343df3d7b7bd30cdb
Altaf Quadri <altafmquadri@gmail.com>
2019-10-26 14:48:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+var var1 = 31 
+var var2 = 2
+let multiply = var1 * var2
+
+const random = Math.random();
+
+const mod = 16 % 6
+
+const max = Math.max(8, 12, 15, 20, 17, 3)
