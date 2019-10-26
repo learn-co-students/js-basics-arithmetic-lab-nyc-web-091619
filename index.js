3a0dfc8dedaa79eefa125bebe0dfabe418aba1d3
Danny Lee <dreamer@panix.com>
2019-10-26 09:56:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here
+
+let var1 = 2;
+let var2 = 31;
+let multiply = var1 * var2 ;
+let random = Math.floor(Math.random() * 10) +1;
+let mod = 12 % 8;
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20);
