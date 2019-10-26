f9ccb9b3b94541d9fdde2fd65988ead7c517ff68
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-26 14:36:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+const var1 = 1;
+const var2 = 62;
+const multiply = var1 * var2;
+
+
+const random = Math.floor(Math.random() * 10) +1;
+
+const mod = 16 % 6;
+
+const max = Math.max(2, 3, 5, 20);
