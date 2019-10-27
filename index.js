66d77b624ad16a5ebffdd46ae5b415ea402d5aca
Felix <felixaviermartinez@gmail.com>
2019-10-27 14:43:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+let var1 = 31
+
+let var2 = 2
+
+let multiply = var1 * var2
+
+let random = Math.random(1, 10);
+
+let mod = (10 - 6)
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
