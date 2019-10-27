3718a9bf38e97461ac3e259f7bcdb9f0f78e9e9e
AdamBrownBB <adam.brown.betty@gmail.com>
2019-10-27 18:54:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here
+const var1 = 2
+const var2 = 31
+let multiply = var1 * var2;
+let random = Math.floor(Math.random() * 1000) + 1
+let mod = 28 % 6
+let max = Math.max(20, 3, 5)
+
+
